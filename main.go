6a118ad7273d505a9a3e9c8b1e79a40cc1fbb990
Emerson Rocha <emersonroocha96@gmail.com>
2023-03-27 19:57:00 -0300

    Teste para conexões com sites web

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Monitoramento básico de uma site
+
+func main() {
+
+	agora := time.Now() // Coleta o momento da consulta ao site
+	url := os.Args[1]  // parametro de url do site a ser monitorado, exemplo: https://www.google.com
+
+	get, err := http.Get(url) // Pega a informação da URL e retorna um valor X ou algum erro relacionado a consulta.
+
+	if err != nil { // Se a mensagem de erro for diferente de nullo, então o bloco abaixo é executado. 
+		
+		fmt.Println("Ocorreu um erro ao executar o get(url)")
+	    panic(err) // Utilizado a modo de parar imediatamente a aplicação já que um erro ocorreu.
+		
+	}
+
+	decorrido := time.Since(agora).Seconds() // Será feito um calculo de quantos segundos foram decorridos desde o momento da coleta
+	status := get.StatusCode
+
+	fmt.Printf("Status: [%d] | Tempo de Carga: [%f]\n", status, decorrido) // Formatador %d é usado para exibir dados inteiros
+																		  // Formatador %f é usado para um valor flutuante. 
+
+	
+	/*
+	Como executar a consulta na CLI: go run .\main.go https://www.google.com
+	
+	*/
+
+}
